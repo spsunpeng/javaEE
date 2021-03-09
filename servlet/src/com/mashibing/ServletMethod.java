3509992ad1787d1577586b7a23ce7379bd662cb8
company <476567162@qq.com>
2021-03-09 17:28:53 +0800

    company

--- servlet/src/com/mashibing/ServletMethod.java
+++ servlet/src/com/mashibing/ServletMethod.java
@@ -18,4 +18,11 @@ public class ServletMethod extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("do");
     }
+
+    //HttpServlet源码中也是通过service调用doGet/doPost
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("我是service");
+        super.service(req, resp);
+    }
 }
